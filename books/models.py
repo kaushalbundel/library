7c73796947b8bb12fca45eb7eb87023f30ff8912
Kaushal Bundel <kaushalbundel@gmail.com>
2024-07-14 01:49:07 +0530

    Basic Django app created
    Created Model, Views and Urls for the books app. Also created a new app called api which will contain all the api related code

--- books/models.py
+++ books/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Book(models.Model):
+    author = models.CharField(max_length=250)
+    subtitle = models.CharField(max_length=250)
+    title = models.CharField(max_length=250)
+    isbn = models.CharField(max_length=13)
+
+    def __str__(self) -> str:
+        return self.title
--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,8 +38,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # third party
+    "rest_framework",
     # user defined
-    "books",
+    "books.apps.BooksConfig",
+    "api.apps.ApiConfig",
 ]
 
 MIDDLEWARE = [
@@ -56,7 +60,7 @@ ROOT_URLCONF = "config.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
